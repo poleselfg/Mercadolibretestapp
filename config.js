ed0514537a2b0dffc206e3e73279f225cf73f8e8
Martin Diaz <martin.diaz@mercadolibre.com>
2019-05-20 18:17:19 -0300

    Just add the sdk version

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
 var config = {
 
 	api_root_url: 'https://api.mercadolibre.com',
-	skd_version: '0.0.14',
+	skd_version: 'MELI-NODE-SDK-0.0.15',
 	auth_url: 'https://auth.mercadolibre.com/authorization',
 	oauth_url: 'https://api.mercadolibre.com/oauth/token',
 	client_id :  process.env.App_ID,
